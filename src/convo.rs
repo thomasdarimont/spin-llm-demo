63162da08748ed00875a757a168991127e7892f6
Thomas Darimont <thomas.darimont@googlemail.com>
2023-11-26 17:15:17 +0100

    initial import

---
+++ src/convo.rs
@@ -0,0 +1,81 @@
+use std::fmt::format;
+use anyhow::Error;
+use serde::{Deserialize, Serialize};
+use spin_sdk::key_value::Store;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Conversation {
+    pub id: String,
+    pub interactions: Vec<Interaction>,
+}
+
+const SYSTEM_INSTRUCTION: &str = r#"
+<<SYS>>
+You are an AI sidekick to help users with extending their knowledge about geography.
+Keep your answers as short as possible.
+"#;
+
+/*
+
+request -> text
+--> 1st interaction?
+--> create or load convo
+--> get prompt()
+--> register_interaction(input, output)
+--> store convo
+
+ */
+
+impl Conversation {
+    pub fn new(id: String) -> Self {
+        Conversation {
+            id,
+            interactions: vec![],
+        }
+    }
+
+    pub fn get_prompt(&self, input: &str) -> String {
+        let mut prompt = String::new();
+        prompt.push_str("<s>[INST]");
+        prompt.push_str(SYSTEM_INSTRUCTION);
+        prompt.push_str("[/INST]</s>\n");
+        for interaction in &self.interactions {
+            prompt.push_str(&interaction.get_prompt());
+        }
+        prompt.push_str(format!("<s>[INST]{}[/INST]</s>\n", input).as_str());
+        prompt
+    }
+
+    pub fn store(&self) -> anyhow::Result<()> {
+        let store = Store::open_default()?;
+        store.set_json(&self.id, self);
+        Ok(())
+    }
+
+    pub fn load(id: &str) -> anyhow::Result<Self> {
+        let store = Store::open_default()?;
+        match store.get_json::<Conversation>(id)? {
+            None => Ok(Conversation::new(id.to_string())),
+            Some(c) => Ok(c)
+        }
+    }
+
+    pub fn add_interaction(&mut self, input: &str, output: &str) {
+        self.interactions.push((Interaction {
+            input: input.to_string(),
+            output: output.to_string(),
+        }))
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Interaction {
+    pub input: String,
+    pub output: String,
+}
+
+impl Interaction {
+    pub fn get_prompt(&self) -> String {
+        format!("<s>\n[INST] {} [/INST] {}\n</s>", self.input, self.output)
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use http::HeaderValue;
+use spin_sdk::http::{IntoResponse, Json, Params, Request, Response, ResponseBuilder, Router};
+use spin_sdk::http_component;
+use spin_sdk::llm;
+use spin_sdk::llm::InferencingModel;
+use uuid::Uuid;
+
+mod prompt;
+mod convo;
+
+use prompt::Prompt;
+use crate::convo::Conversation;
+
+const CONVERSATION_ID_HEADER: &str = "X-ConversationID";
+
+
+/// A simple Spin HTTP component.
+#[http_component]
+fn handle_spin_llm_demo(req: Request) -> anyhow::Result<impl IntoResponse> {
+    let mut router = Router::default();
+    router.post("/", handle_prompt);
+
+    Ok(router.handle(req))
+}
+
+fn handle_prompt(req: http::Request<Json<Prompt>>, _params: Params) -> anyhow::Result<impl IntoResponse> {
+    let text = req.body().text.clone();
+
+    let mut conversation = match req.headers().get(CONVERSATION_ID_HEADER) {
+        Some(id) => {
+            Conversation::load(id.to_str()?)
+        }
+        None => Ok(Conversation::new(Uuid::new_v4().to_string()))
+    }?;
+
+    let prompt = conversation.get_prompt(&text);
+
+    let options = llm::InferencingParams {
+        max_tokens: 100,
+        temperature: 0.7,
+        ..Default::default()
+    };
+
+    let result = llm::infer_with_options(InferencingModel::Llama2Chat, &prompt, options)?;
+
+    conversation.add_interaction(&text, &result.text);
+    conversation.store()?;
+
+    Ok(ResponseBuilder::new(http::status::StatusCode::OK)
+        .header(CONVERSATION_ID_HEADER, &conversation.id)
+        .body(result.text)
+        .build())
+}
+
